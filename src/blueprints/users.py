31bf4f6199dc4ab00abdb46f3ac91d354787e432
Alexey7953 <63520793+Alexey7953@users.noreply.github.com>
2020-06-27 23:29:08 +0500

    fixed

--- src/blueprints/users.py
+++ src/blueprints/users.py
@@ -61,7 +61,7 @@ class UsersView(MethodView):
 
 class UserView(MethodView):
     def get(self, user_id):
-        account_id = session.get('id')
+        account_id = session.get('user_id')
         if not account_id:
             return '', 403
         
